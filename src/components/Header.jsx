7a409eaff3bc07ca37b01b2896275012287adacb
AliTheTechKing <147366716+AliTheTechKing@users.noreply.github.com>
2024-02-28 14:36:19 +0000

    1st Change

---
+++ src/components/Header.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { images } from '../constants'
+
+const Header = () => {
+  return (
+  <section>
+    <header className="container mx-auto px-5 flex justify-between py-4">
+        
+        <div>
+            <img src={images.Logo} alt="logo" />
+        </div>
+        <div className="flex gap-x-9"> 
+            <ul className="flex gap-x-5">
+                <li><a href="/">Home</a></li>
+                <li><a href="/">Articles</a></li>
+                <li><a href="/">Pages</a></li>
+                <li><a href="/">Pricing</a></li>
+                <li><a href="/">Faq</a></li>
+            </ul>
+            <button>Sign in</button>
+        </div>
+    </header>
+  </section>
+  )
+}
+
+export default Header
